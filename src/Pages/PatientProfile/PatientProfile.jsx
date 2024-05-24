d9a2e9dc74f652cd4c34752d3470836733986baa
Raghad Hanon <raghadhanoon2015@gmail.com>
2024-05-24 21:42:55 +0300

    Fix profile

--- src/Pages/PatientProfile/PatientProfile.jsx
+++ src/Pages/PatientProfile/PatientProfile.jsx
@@ -58,7 +58,7 @@ function PatientProfile() {
       const { data } = await axios.get(`/api/patient/${name}`);
       setPatient(data);
       setMedicalData({
-        bloodType: data.bloodType,
+        bloodType:patient.bloodType == "" ? "+O":patient.bloodType ,
         height: data.height,
         weight: data.weight,
       });
@@ -253,7 +253,7 @@ function PatientProfile() {
           </div>
         </div>
 
-        <div className={`mt-4  bgcolor5 rounded-5 border d-flex justify-content-start gap-5`}>
+        <div className={`mt-4 ${style.OverFlow} bgcolor5 rounded-5 border d-flex justify-content-start gap-5`}>
           <ul className={`mb-0  ps-0 d-flex align-self-stretch flex-column justify-content-start bgwhiteC `}>
             <li onClick={() => setPage("info")} className={`py-5 px-3 ${page == "info" ? "bgcolor5" : ""}`}><svg xmlns="http://www.w3.org/2000/svg" height="50" width="50" viewBox="0 0 448 512"><path fill="#176b87" d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-96 55.2C54 332.9 0 401.3 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7c0-81-54-149.4-128-171.1V362c27.6 7.1 48 32.2 48 62v40c0 8.8-7.2 16-16 16H336c-8.8 0-16-7.2-16-16s7.2-16 16-16V424c0-17.7-14.3-32-32-32s-32 14.3-32 32v24c8.8 0 16 7.2 16 16s-7.2 16-16 16H256c-8.8 0-16-7.2-16-16V424c0-29.8 20.4-54.9 48-62V304.9c-6-.6-12.1-.9-18.3-.9H178.3c-6.2 0-12.3 .3-18.3 .9v65.4c23.1 6.9 40 28.3 40 53.7c0 30.9-25.1 56-56 56s-56-25.1-56-56c0-25.4 16.9-46.8 40-53.7V311.2zM144 448a24 24 0 1 0 0-48 24 24 0 1 0 0 48z" /></svg></li>
             <li onClick={() => setPage("dailyCheck")} className={`py-5 px-3 ${page == "dailyCheck" ? "bgcolor5" : ""}`}><svg xmlns="http://www.w3.org/2000/svg" height="50" width="50" viewBox="0 0 448 512"><path fill="#176b87" d="M128 0c17.7 0 32 14.3 32 32V64H288V32c0-17.7 14.3-32 32-32s32 14.3 32 32V64h48c26.5 0 48 21.5 48 48v48H0V112C0 85.5 21.5 64 48 64H96V32c0-17.7 14.3-32 32-32zM0 192H448V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V192zm64 80v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm128 0v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H208c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H336zM64 400v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H208zm112 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H336c-8.8 0-16 7.2-16 16z" /></svg></li>
